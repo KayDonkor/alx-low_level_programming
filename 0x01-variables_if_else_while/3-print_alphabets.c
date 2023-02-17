6247add0516efa534853397d4b72afb20ae56eb8
KayDonkor <kadonkor98@gmail.com>
2023-02-17 15:26:31 -0800

    lower and uppercase alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-        putchar(alphabet);
+	putchar(alphabet);
 	}
 	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 	{
