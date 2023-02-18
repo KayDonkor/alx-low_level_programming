6b347d8f8773447aa286c9385429305dd167a91c
KayDonkor <kadonkor98@gmail.com>
2023-02-17 16:04:30 -0800

    under 10 numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- * main- prints all single digit numbers of base 10
+ * main - prints all single digit numbers of base 10
  * starting from 0, followed by a new line
  * Return: 0
  */
@@ -11,7 +11,7 @@ int num;
 
 	for (num = '0'; num <= '9'; num++)
 	{
-	putchar(num+0);
+	putchar(num + 0);
 	}
 putchar('\n');
 return (0);
