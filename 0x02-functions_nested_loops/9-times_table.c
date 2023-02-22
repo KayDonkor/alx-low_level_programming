7f217ee8b293fc4ca3df4f2521d0235af84c5495
KayDonkor <kadonkor98@gmail.com>
2023-02-21 18:58:47 -0800

    nine times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,15 +9,15 @@
 void times_table(void)
 {
 	int i;
-       int j;
-      int k;
+	int j;
+	int k;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-			if ( j == 0)
+			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
