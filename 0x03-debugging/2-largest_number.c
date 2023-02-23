395a0b4595c1d74aedd1c0b4a8514fba9aa64d50
KayDonkor <kadonkor98@gmail.com>
2023-02-23 11:32:06 -0800

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -36,9 +36,13 @@ int largest_number(int a, int b, int c)
 	{
 	largest = c;
 	}
-	else
+	else if (c > b && b > a)
 	{
 	largest = c;
+	}
+	else 
+	{
+	printf("Multiple largest numbers");
 	}
 
 	return (largest);
