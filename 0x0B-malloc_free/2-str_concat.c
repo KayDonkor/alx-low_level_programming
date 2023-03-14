c843749a46a7ffc867f2ebba09172d9d302780d5
KayDonkor <kadonkor98@gmail.com>
2023-03-14 15:53:10 -0700

    malloc wahala

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,16 +9,17 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-
-	int i, ci;
+	int i;
+	int ci;
 
 	if (s1 == NULL)
-		s1 = "";
+	s1 = "";
+
 	if (s2 == NULL)
-	{
-		s2 = "";
-		i = ci = 0;
-	}
+	s2 = "";
+
+	i = ci = 0;
+
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
